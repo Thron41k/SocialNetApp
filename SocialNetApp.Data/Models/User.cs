198d05dae1f578688b61eb10c57e10c2597238b7
Timothy <Thron41k@gmail.com>
2025-06-04 06:29:13 +0800

    The work is finished

--- SocialNetApp.Data/Models/User.cs
+++ SocialNetApp.Data/Models/User.cs
@@ -25,6 +25,12 @@ public class User : IdentityUser
     {
         return BirthDate.ToString("dd MMMM yyyy г.");
     }
+    public User()
+    {
+        Image = "https://dummyimage.com/500x500/a6a6a6/000000.png";
+        Status = "Ура! Я в соцсети!";
+        About = "Информация обо мне.";
+    }
     public virtual ICollection<Friend> Friends { get; set; }
 
 }
